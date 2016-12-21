6435ff48ccfbc9b56d9557c4b8246265f7b84d0d
George Brocklehurst <george@georgebrock.com>
2016-12-21 13:03:02 +0000

    Ruby: Basic tab completion.

--- main.rb
+++ main.rb
@@ -1,5 +1,17 @@
 require "readline"
 
+CHARACTER_NAMES = [
+  "Arthur Dent",
+  "Ford Prefect",
+  "Tricia McMillan",
+  "Zaphod Beeblebrox",
+].freeze
+
+Readline.completion_proc = proc do |input|
+  CHARACTER_NAMES.select { |name| name.start_with?(input) }
+end
+
+puts "Who's your favourite Hitchiker's Guide character?"
 input = Readline.readline("> ", false)
 if input
   puts "You entered: #{input}"
